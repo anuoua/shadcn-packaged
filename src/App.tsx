a016bb97c58833e500d89c98001fa9ff72af581c
anuoua <anuoua@gmail.com>
2025-01-16 07:48:35 +0000

    vite init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./shadcn/ui/button"
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <Button>
+        Hello Shadcn
+      </Button>     
+    </div>
+  )
+}
+
+export default App
